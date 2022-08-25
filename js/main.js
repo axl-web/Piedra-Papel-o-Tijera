d66d150005a5624eab76c91a79fd28fda32249e5
Axl-Web <montenegroaxel@protonmail.com>
2022-08-24 21:41:55 -0300

    desarollo de funciones Eleccion y Combate

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
 //1 es piedra, 2 es papel y 3 es tijera
 
 let jugador = 0;
-let ia = 0;
+let ia = Math.floor(Math.random() * 3) + 1;
 
 jugador = prompt(
   "Elije una opcion" + "\n1. Piedra 🪨" + "\n2. Papel  📄" + "\n3. Tijera  ✂️"
 );
 
-eleccion(jugador);
+eleccion(jugador, "Jugador");
+eleccion(ia, "PC");
+combate(jugador, ia);
 
-function eleccion(eleccion) {
-  if (eleccion >= 1 && eleccion <= 3) {
-    alert("Elejiste " + jugador);
+function eleccion(opcion, player) {
+  if (opcion == 1) {
+    alert(player + " Elijio 🪨");
+  } else if (opcion == 2) {
+    alert(player + " Elijio 📄");
+  } else if (opcion == 3) {
+    alert(player + " Elijio ✂️");
   } else {
-    alert("Tu eleccion es incorrecta");
+    alert(
+      "No ingresaste una opcion correcta." +
+        "\n Presiona F5 para volver a jugar"
+    );
+  }
+}
+
+function combate(player, pc) {
+  if (player == pc) {
+    alert("EMPATE" + "\n Presiona F5 para volver a jugar");
+  } else if (player == 1 && pc == 3) {
+    alert("TU GANAS" + "\n Presiona F5 para volver a jugar");
+  } else if (player == 2 && pc == 1) {
+    alert("TU GANAS" + "\n Presiona F5 para volver a jugar");
+  } else if (player == 3 && pc == 2) {
+    alert("TU GANAS" + "\n Presiona F5 para volver a jugar");
+  } else {
+    alert("TU PIERDES" + "\n Presiona F5 para volver a jugar");
   }
 }
